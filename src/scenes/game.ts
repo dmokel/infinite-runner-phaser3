a0872981e96ede6d2cf655692955d1a2347d2d2c
David Mokel <Moqqll.china@gmail.com>
2023-06-28 16:01:59 +0800

    feat(game): physics feature support and ininite running visual effect

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -4,6 +4,8 @@ import SceneKeys from '../consts/scenekeys';
 import TextureKeys from '../consts/texturekeys';
 
 export default class Game extends Phaser.Scene {
+  private backbround!: Phaser.GameObjects.TileSprite;
+
   constructor() {
     super(SceneKeys.Game);
   }
@@ -13,17 +15,41 @@ export default class Game extends Phaser.Scene {
     const width = this.scale.width;
     const height = this.scale.height;
 
-    this.add
+    this.backbround = this.add
       .tileSprite(0, 0, width, height, TextureKeys.Background)
-      .setOrigin(0);
+      .setOrigin(0)
+      .setScrollFactor(0, 0);
 
-    this.add
+    // this.add
+    //   .sprite(
+    //     width * 0.5,
+    //     height * 0.5,
+    //     TextureKeys.RocketMouse,
+    //     'rocketmouse_fly01.png'
+    //   )
+    //   .play(AnimationKeys.RocketMouseRun);
+
+    const mouse = this.physics.add
       .sprite(
         width * 0.5,
-        height * 0.5,
+        height - 30,
         TextureKeys.RocketMouse,
         'rocketmouse_fly01.png'
       )
+      .setOrigin(0.5, 1)
       .play(AnimationKeys.RocketMouseRun);
+
+    const body = mouse.body as Phaser.Physics.Arcade.Body;
+    body.setCollideWorldBounds(true);
+    body.setVelocityX(200);
+
+    this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30);
+
+    this.cameras.main.startFollow(mouse);
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height);
+  }
+
+  update(t, dt) {
+    this.backbround.setTilePosition(this.cameras.main.scrollX);
   }
 }
